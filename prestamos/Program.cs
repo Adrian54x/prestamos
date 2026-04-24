76671c8a8800f783801d1e345b2c8a43b0da02b4
adria <adria@REX54X>
2026-04-24 08:23:16 -0600

    Class

--- prestamos/Program.cs
+++ prestamos/Program.cs
@@ -1,1 +1,52 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+class Control
+{
+    private int codigoPrestamo;
+    private string nombreEstudiante;
+    private int carnet;
+    private string carrera;
+    private string equipoPrestado;
+    private int cantidad;
+    private bool estadoPrestamo;
+
+    public int CodigoPrestamo
+    {
+        get { return codigoPrestamo; }
+        set { codigoPrestamo = value; }
+    }
+    public string NombreEstudiante
+    {
+        get { return nombreEstudiante; }
+        set { nombreEstudiante = value; }
+    }
+    public int Carnet
+    {
+        get { return carnet; }
+        set { carnet = value; }
+    }
+    public string Carrera
+    {
+        get { return carrera; }
+        set { carrera = value; }
+    }
+    public string EquipoPrestado
+    {
+        get { return equipoPrestado; }
+        set { equipoPrestado = value; }
+    }
+    public int Cantidad
+    {
+        get { return cantidad; }
+        set { cantidad = value; }
+    }
+    public bool EstadoPrestamo
+        {
+        get { return estadoPrestamo; }
+        set { estadoPrestamo = value; }
+    }
+
+    public Control(int CodigoPrestamo, string NombreEstudiante, int Carnet, string Carrera, string EquipoPrestado, int Cantidad, bool EstadoPrestamo)
+    { 
+    }
+}
